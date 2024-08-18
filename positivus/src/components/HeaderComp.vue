066b588f19bba6e8df942b6965a97406d75937fa
олег виницкий <29-vinitskiy-vo@mail.ru>
2024-08-18 15:58:01 +0700

    добавил хедер

---
+++ positivus/src/components/HeaderComp.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="header center">
+      <!-- <h1>{{ msg }}</h1> -->
+      <nav class="header__left">
+        <img src="../assets/star.svg" alt="">
+        <img class="header__left__logoText" src="../assets/logo.svg" alt="">
+      </nav>
+      <nav class="header__right">
+        <a class="header__right__nav" href="#">About us</a>
+        <a class="header__right__nav" href="#">Services</a>
+        <a class="header__right__nav" href="#">Use Cases</a>
+        <a class="header__right__nav" href="#">Pricing</a>
+        <a class="header__right__nav" href="#">Blog</a>
+        <button class="header__right__btn">Request a quote</button>
+      </nav>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeaderComp',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style lang="scss">
+  @import "../styles/variables.scss";
+  @import '../styles/mixins.scss';
+    a {
+      text-decoration: none;
+    }
+    .header {
+      @include center;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      &__left {
+        @include centering;
+        &__logoText {
+          padding-left: 13px;
+          cursor: pointer;
+        }
+      }
+      &__right {
+        display: flex;
+        align-items: center;
+        gap: 40px;
+        &__nav {
+          color: rgb(0, 0, 0);
+          font-family: Space Grotesk;
+          font-size: 20px;
+          font-weight: 400;
+          line-height: 28px;
+        }
+        &__btn {
+          box-sizing: border-box;
+          border: 1px solid rgb(25, 26, 35);
+          border-radius: 14px;
+          color: rgb(0, 0, 0);
+          font-family: Space Grotesk;
+          font-size: 20px;
+          font-weight: 400;
+          line-height: 28px;
+          background-color: white;
+          padding: 20px 35px;
+          cursor: pointer;
+        }
+      }
+    }
+  
+  </style>
---
+++ positivus/src/components/StartInfo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="startInfo center">
+        <div class="startInfo__left">
+            <h1 class="startInfo__left__title">Navigating the digital landscape for success</h1>
+            <p class="startInfo__left__text">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+            <button class="startInfo__left__btn">Book a consultation</button>
+        </div>
+        <div class="startInfo__right">
+            <img src="../assets/Illustration.svg" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'StartInfo',
+
+    }
+</script>
+
+<style lang="scss">
+    @import "../styles/variables.scss";
+    @import '../styles/mixins.scss';
+    .startInfo {
+        @include center;
+        display: flex;
+        margin-top: 70px;
+        &__left {
+            display: flex;
+            flex-direction: column;
+        }
+    }
+
+</style>
